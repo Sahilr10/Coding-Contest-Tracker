cfac53fabfdb96db516e07ee6bd8600ff030c5e0
Sahil Raut <rautsahil102005@gmail.com>
2025-09-15 19:32:18 +0530

    Implement user authentication with login and logout functionality, add JWT verification middleware

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,7 +1,23 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { APiError } from "../utils/ApiError.js";
+import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js"; 
-import User from "../models/user.model.js";
+import {User} from "../models/user.model.js";
+
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken;
+        await user.save({validateBeforeSave: false});
+
+        return {accessToken, refreshToken};
+
+    } catch (error) {
+        throw new ApiError(500, 'Something went wrong while generating tokens');
+    }
+}
 
 const registerUser = asyncHandler(async (req, res) => {
     
@@ -10,11 +26,11 @@ const registerUser = asyncHandler(async (req, res) => {
     if(
         [username, email, password].some((field)=> field?.trim() === '' )
     ){
-        throw new APiError('All fields are required', 400);
+        throw new ApiError('All fields are required', 400);
     }
 
     if(!email.includes('@')){
-        throw new APiError('Invalid email', 400);
+        throw new ApiError('Invalid email', 400);
     }
 
     const existedUser = await User.findOne({
@@ -22,7 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
     })
 
     if(existedUser){
-        throw new APiError(409,'User already exists');
+        throw new ApiError(409,'User already exists');
     }
 
     const user = await User.create({
@@ -42,4 +58,73 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, 'User created successfully'))
 })
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+    const {email, password} = req.body
+
+    if([email, password].some((field)=> field?.trim() === '' )){
+        throw new ApiError(400,'All fields are required');
+    }
+
+    const user = await User.findOne({email})
+
+    if(!user){
+        throw new ApiError(404,'User not found');
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(401,'Invalid credentials');
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select('-password -refreshToken')
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options) 
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser,
+                accessToken,
+                refreshToken
+            },
+             'User logged in successfully'
+        ))
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset: {refreshToken: 1}
+        },
+        {new: true}
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, 'User logged out successfully'))
+})
+
+export { 
+    registerUser,
+    loginUser,
+    logoutUser
+    };
